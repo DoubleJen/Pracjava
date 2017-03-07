bf0a313267647c53fe4e67036b1daa106756a71d
yujen <yujen@YUJEN_PC>
2017-03-07 23:19:16 +0800

    Hellodouble001

---
+++ Pracjava170307001/src/tw/pracdou001/Hellodouble001.java
@@ -0,0 +1,29 @@
+package tw.pracdou001;
+		//1st line can exist or not.
+
+public class Hellodouble001 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, Double");
+		System.out.println("Hello, Rainyday");
+	
+		// "//" is single row note.
+		
+		/*
+		 *  "/* + Enter" is multiple row notes.
+		 */
+
+		//"sysout + alt + /" quicks to System.out.println();
+		
+		/*
+		 * "@XXXXX" is Javadoc mark. 
+		 */
+
+		/*
+		 * TODO tasks
+		 * "todo" is todo list, call fr "Windows/Show View/...", 
+		 * such as "Tasks(with todo)", "Console(with Output)", "Package Explorer(with Outline)"...
+		 */
+	}
+
+}
