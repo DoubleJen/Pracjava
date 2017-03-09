4d49442650f74efab212eed8b74b0993b3a886b0
Mac <Mac@Mac-PC>
2017-03-09 21:20:06 +0800

    20170309001

--- Pracjava170307001/src/tw/pracdou001/Type001.java
+++ Pracjava170307001/src/tw/pracdou001/Type001.java
@@ -12,22 +12,41 @@ public class Type001 {
 		
 		//big5 = 2bytes = 2^16 = 65536 ; UTF8=2^24 
 		int $_$=1234567890;
-		System.out.println($_$);
+		System.out.println($_$);//-->1234567890
 		
 		
-		//byte(2^8), short(2^16), int(2^32), long(2^64)
+		//整數型態byte(2^8), short(2^16), int(2^32), long(2^64)
 		byte var1=111;
 		byte var2, var3=127, var4, var5=-11;
-			//var2/4無給值稱無初始化-->error var2/4 may not have been initialized.-->無法運算
-		System.out.println(var1+var5);
+			//var2,4無給值稱無初始化-->error var2,4 may not have been initialized.-->無法運算
+		System.out.println(var1);//-->111
+		System.out.println(var1+var5);//-->100
+		System.out.println(var1-var5);//-->122
+		System.out.println(var1*var5);//-->-1221
+		System.out.println(var1/var5);//-->-10?
 		
 		var5 ++;
-			//++ is 遞增運算子(向前位移),http://pydoing.blogspot.tw/2010/10/java-operator.html
-		System.out.println(var5);
+			//++ is 遞增運算子(位移), http://pydoing.blogspot.tw/2010/10/java-operator.html
+		System.out.println(var5);//-->-10
 		
-		var5 +=10;
-			//TODO += is 
-		System.out.println(var5);
+		var5 --;
+			//-- is 遞減運算子(位移), http://pydoing.blogspot.tw/2010/10/java-operator.html
+			//var5 line25已遞增過一次, var5 line29會以line25的結果進行遞減
+		System.out.println(var5);//-->-11
+		
+		var5 +=5;
+			//+= is 相加同時指派 , http://pydoing.blogspot.tw/2010/10/java-operator.html
+			//-11+5=-6
+		System.out.println(var5);//-->-6
+		
+		var5 = (byte)(3 + var5);
+			/*
+			 * 3 & var5單位不同, 因此需要加上"(byte, short, int, long...)"強制轉換為byte, short, int, long...
+			 */
+		System.out.println(var5);//>-3
+		
+	
+
 		
 	}
 
