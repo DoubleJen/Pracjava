10294ab8a783e218ff920d5853ade9ea81af7a96
yujen <yujen@YUJEN_PC>
2017-03-18 01:33:40 +0800

    20170318

---
+++ Pracjava170307001/src/tw/pracdou001/Check_NumtoChinese.java
@@ -0,0 +1,16 @@
+package tw.pracdou001;
+
+public class Check_NumtoChinese {
+
+	public static void main(String[] args) {
+		int[] num = new int[10];
+		switch(num){
+			case num[0]:
+				System.out.println("拾億");
+				
+		
+		}
+
+	}
+
+}
