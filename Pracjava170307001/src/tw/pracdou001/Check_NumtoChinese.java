3a815d9a25a524daf4994b5e72ccc3fa327708fc
Mac <Mac@Mac-PC>
2017-03-18 17:09:03 +0800

    20170318

--- Pracjava170307001/src/tw/pracdou001/Check_NumtoChinese.java
+++ Pracjava170307001/src/tw/pracdou001/Check_NumtoChinese.java
@@ -3,9 +3,9 @@ package tw.pracdou001;
 public class Check_NumtoChinese {
 
 	public static void main(String[] args) {
-		int[] num = new int[10];
+		int num = 10;
 		switch(num){
-			case num[0]:
+			case 10:
 				System.out.println("拾億");
 				
 		
