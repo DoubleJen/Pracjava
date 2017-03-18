494f4da60bee0f70e229009dd9122e73e3e3727e
yujen <yujen@YUJEN_PC>
2017-03-19 01:19:34 +0800

    20170319

--- Pracjava170307001/src/tw/pracdou001/Check_NumtoChinese.java
+++ Pracjava170307001/src/tw/pracdou001/Check_NumtoChinese.java
@@ -1,16 +1,32 @@
 package tw.pracdou001;
 
+import javax.swing.JOptionPane;
+
 public class Check_NumtoChinese {
 
 	public static void main(String[] args) {
-		int num = 10;
-		switch(num){
-			case 10:
-				System.out.println("拾億");
+		String input = JOptionPane.showInputDialog("請輸入數字");
+		char a = '0';
+		for(int i=0; i<input.length(); i++){
+			a = input.charAt(i);
+			
+		}
+		System.out.println(a);
+		switch(a){
+				case '1': System.out.println("壹"); break;
+				default: System.out.println("空空如也"); break;
+			}
+
+		
+		
+//		int num = 10;
+//		switch(num){
+//			case 10:
+//				System.out.println("拾億");
 				
 		
-		}
-
 	}
 
 }
+
+
