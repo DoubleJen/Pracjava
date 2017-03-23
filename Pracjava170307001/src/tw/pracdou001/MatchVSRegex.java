89c567bf03280c891b22ee532d29c3cafc0075d7
yujen <yujen@YUJEN_PC>
2017-03-23 13:37:43 +0800

    20170323

---
+++ Pracjava170307001/src/tw/pracdou001/MatchVSRegex.java
@@ -0,0 +1,13 @@
+package tw.pracdou001;
+//match V.S. Regular Expression(正規表示法)
+//https://zh.wikipedia.org/zh-tw/%E6%AD%A3%E5%88%99%E8%A1%A8%E8%BE%BE%E5%BC%8F
+//http://www.webpage.idv.tw/maillist/maillist4/pro/05/regular1.htm
+public class MatchVSRegex {
+
+	public static void main(String[] args) {
+		String s1 = "1234-56789012";
+		//System.out.println(s1.matches("^((\d{3,4}-)|\d{3.4}-)?\d{7,8}$"));
+
+	}
+
+}
