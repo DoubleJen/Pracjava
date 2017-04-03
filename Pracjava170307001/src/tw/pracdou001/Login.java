52647ec06cb95ab8246d103a73f7e2a1224c32af
yujen <yujen@YUJEN_PC>
2017-04-04 00:29:35 +0800

    20170404

--- Pracjava170307001/src/tw/pracdou001/Login.java
+++ Pracjava170307001/src/tw/pracdou001/Login.java
@@ -1,7 +1,9 @@
 package tw.pracdou001;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Font;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -9,6 +11,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import javax.swing.ToolTipManager;
 
 public class Login extends JFrame {
 	JLabel ID = new JLabel("User Name");
@@ -21,28 +24,34 @@ public class Login extends JFrame {
 	JButton Clear = new JButton("CLEAR");
 	
 	private ButtonHandler ButtonEvent = new ButtonHandler();
-	private class ButtonHandler{
-		
+	private class ButtonHandler{		
 	}
+	
+	
 	
 	Login(){
 		super("請輸入帳號密碼");
-		setSize(300, 180);
+		setSize(260, 240);
+		setLocation(350, 350);
 		setVisible(true);
 		//setResizable(false);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
-		
 		JPanel jp = new JPanel();
 		jp.setBackground(Color.white);
-		jp.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		jp.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
 		jp.add(ID);
 		jp.add(IDInput);
 		jp.add(Pass);
 		jp.add(PassInput);
 		jp.add(Enter);
 		jp.add(Clear);
+		ID.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 20));
+		Pass.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 20));
+		PassInput.setEchoChar('*');
+		PassInput.setToolTipText("密碼長度6位以上、小於20位，限a-z、A-Z、0-9、$_*%?^");
 		setContentPane(jp);
+		
 	}
 	
 	
