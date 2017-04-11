c288c4a9037e5b9c7ec92d567347abbd63ac7673
Mac <Mac@Mac-PC>
2017-04-11 21:29:44 +0800

    20170411

--- Pracjava170307001/src/tw/pracdou001/Test1.java
+++ Pracjava170307001/src/tw/pracdou001/Test1.java
@@ -1,8 +1,27 @@
 package tw.pracdou001;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class Test1 {
 
 	public static void main(String[] args) {
+		File fin = new File("./DIREC1/test.txt");
+		try {
+			FileReader fr = new FileReader(fin);
+			//System.out.println(fr.read());
+			int frr;
+			while((frr = fr.read())!=-1){
+				System.out.print((char)frr);
+				
+			}
+			
+			//System.out.println("11");
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+		
 		
 
 	}
